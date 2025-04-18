584cc51b34c2034b30a998026321b4687da9c6a0
haneen-elghawy <148449014+haneen-elghawy@users.noreply.github.com>
2025-04-19 01:14:15 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,10 +2,10 @@ import { Low, JSONFile } from 'lowdb';
 
 const db = new Low(new JSONFile('db.json'));
 
-// Set up default data if it doesn't exist
+
 db.data ||= { users: [], posts: [] };
 
-// Function to initialize the db and read data
+
 const initializeDB = async () => {
   await db.read();
 };
